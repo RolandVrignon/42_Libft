028b862573afc9386693175dbd70994d2ad42844
RolandVrignon <roland.vrignon@gmail.com>
2022-04-30 02:13:43 +0200

    minior changes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,26 +6,25 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 17:37:52 by rvrignon          #+#    #+#             */
-/*   Updated: 2022/04/30 00:02:36 by rvrignon         ###   ########.fr       */
+/*   Updated: 2022/04/30 02:12:27 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdint.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	size_t	i;
 
-	i = 0;
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	while (i < nmemb * size)
-	{
-		((char *)ptr)[i] = 0;
-		i++;
-	}
-	return (ptr);
+	if ((nmemb == SIZE_MAX || size == SIZE_MAX) && (nmemb > 1 || size > 1))
+		return ((void *) NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero((char *)ptr, nmemb * size);
+	return ((char *) ptr);
 }
